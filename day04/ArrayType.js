2c79224fb981d5b661d432f6e6f625b02faa07bc
Arman Ali <armnsrn173200@gmail.com>
2023-10-16 23:57:06 +0530

    Initial commit

--- day04/ArrayType.js
+++ day04/ArrayType.js
@@ -1,19 +1,20 @@
-//Types of Array on the basis  of implementation
-//continious/Packed, Holey Array Both have 3 types 
+// //Types of Array on the basis  of implementation
+// //continious/Packed, Holey Array Both have 3 types 
 
-//1. SMI(small integer)
+// //1. SMI(small integer)
 
-const arr=[1,2,3,4,5,6]
-//2-Packed double Elements
-arr.push(9.1);
-//it become-[1,2,3,4,5,6,9.1]
-//3-packed elements
-arr.push('23');
-//holey elements
-arr[123]=12
-const a=[1,2,3,4,5]
-a.push(9.1);
-a.push('7')
-a[123]=12
-console.log(a[20]);
-console.log(a[202]);
+// const arr=[1,2,3,4,5,6]
+// //2-Packed double Elements
+// arr.push(9.1);
+// //it become-[1,2,3,4,5,6,9.1]
+// //3-packed elements
+// arr.push('23');
+// //holey elements
+// arr[123]=12
+// const a=[1,2,3,4,5]
+// a.push(9.1);
+// a.push('7')
+// a[123]=12
+// console.log(a[20]);
+// console.log(a[202]);
+//prime number
