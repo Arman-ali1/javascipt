84ee3913b3862ed833dd4313e668ac757abb9202
Arman Ali <armnsrn173200@gmail.com>
2023-10-25 19:23:13 +0530

    Initial commit

---
+++ day10/practice.js
@@ -0,0 +1,8 @@
+console.log("start");
+setTimeout(() => {
+    console.log("Settimeout-0");
+}, 1);
+setTimeout(() => {
+    console.log("Settimeout");
+},0);
+console.log("end");
---
+++ day11/Vriable.js
@@ -0,0 +1,26 @@
+// VARIABLES IN JAVASCRIPT
+
+
+
+const accountId = 144553
+let accountEmail = "xyz@google.com"
+var accountPassword = "12345"
+accountCity = "xyzr"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "ar12@ar.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
